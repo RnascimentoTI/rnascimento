b4215a95e5d233b7f04ec6b7bf285f9b7650db4f
RnascimentoTI <104452606+RnascimentoTI@users.noreply.github.com>
2022-04-26 13:48:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+/* abre e fecha o menu quando clicar no icon 3 risco e o X */
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+
+/* Quando clicar em um item do menu , esconder o menu */
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    // menor que a altura do header
+    header.classList.remove("scroll");
+  }
+});
+
+/* testimonials carousel slider swiper */
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+/* Botão voltar para o topo */
+
+const backToTopButton = document.querySelector(".back-to-top");
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classlist.remove("show");
+  }
+});
